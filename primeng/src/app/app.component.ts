8545d1abe11b6271d8403c12cd06c2ff6f7b6092
Juan Carlos Forero <juan.forero66@gmail.com>
2024-10-21 11:06:17 -0500

    trabajo en clase

---
+++ primeng/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from './components/layout/menu/menu.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    MenuComponent,
+    FooterComponent
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  text = 'Hello World';
+}
